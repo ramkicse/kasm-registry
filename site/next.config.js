55610a8d1b8769a7f6ee0189c0d69f92c2ce560f
Ramakrishnan E.P <ramkicse@gmail.com>
2023-09-06 12:16:59 +0530

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
+    name: 'Ramakrishnan Registry',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: ' https://ramkicse.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/ramkicse/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
